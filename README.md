a817252c99b17b02ffb0ee1aa121d5ac9b3702f6
Leo Shiang <leoshiang@gmail.com>
2024-06-19 15:26:48 +0800

    初始版本。

---
+++ README.md
@@ -0,0 +1,23 @@
+# TGMsg
+
+這是一個傳送 Telegram 的命令列工具，支援 Windows/Linux/MacOS。
+
+## 使用方式
+
+```text
+TGMsg -c chatid -t token -m message
+```
+
+**選項**
+```text
+-c  chatid
+-t  token
+-m  message 
+```
+
+### 範例
+
+
+```bash
+TGMsg -c 123123123 -t XXXX -m "Hello"
+```
---
+++ index.js
@@ -0,0 +1,20 @@
+const { program } = require('commander')
+
+program
+    .name('TGMsg')
+    .description('CLI to send telegram message.')
+    .option('-c, --chatid <number>', 'Chat ID')
+    .option('-t, --token <string>', 'Token')
+    .option('-m, --message <string>', '要傳送的訊息')
+
+program.parse(process.argv)
+const options = program.opts()
+
+const url = `https://api.telegram.org/bot${options.token}/sendMessage?chat_id=${options.chatid}&text=${options.message}`
+fetch(url)
+    .then(function (response) {
+        console.log(response)
+    })
+    .catch(function (error) {
+        console.log(error)
+    })
